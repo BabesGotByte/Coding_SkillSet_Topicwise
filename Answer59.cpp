65361883b9156af3b37aaa5e253778e3dca3998e
Vikram Vidhan Singh <vikramvidhansingh@gmail.com>
2022-10-27 20:14:19 +0530

    59added

---
+++ Answer59.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+#define MAX 10001
+
+int parent[MAX];
+int sz[MAX];
+
+
+
+void make_set(int v) {
+    parent[v] = v;
+    sz[v] = 1;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (sz[a] < sz[b])
+            swap(a, b);
+        parent[b] = a;
+        sz[a] += sz[b];
+    }
+}
+
+int main(){
+
+}
