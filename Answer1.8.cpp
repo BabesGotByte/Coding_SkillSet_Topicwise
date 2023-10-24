8ce13f4f010c921b9f5e72c0821a3d75677c8040
BairagiTarun <112105779+BairagiTarun@users.noreply.github.com>
2023-10-24 11:40:01 +0530

    Update Answer1.8.cpp

--- Answer1.8.cpp
+++ Answer1.8.cpp
@@ -2,26 +2,16 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         
-        vector<vector<int>>ans(numRows);
-        
-        
-        for(int i=0;i<numRows;i++)
-        {
-            
-            ans[i].resize(i+1);
-            
-            ans[i][0]=1;
-            ans[i][i]=1;
-            
-            for(int j=1;j<i;j++)
-            {
-                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
-            }
-            
-            
-        }
-        
-        return ans;
+        vector<vector<int>> ans;
+
+    for (int i = 0; i < numRows; ++i)
+      ans.push_back(vector<int>(i + 1, 1));
+
+    for (int i = 2; i < numRows; ++i)
+      for (int j = 1; j < ans[i].size() - 1; ++j)
+        ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+
+    return ans;
         
     }
 };
